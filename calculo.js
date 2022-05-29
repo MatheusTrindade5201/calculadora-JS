2471904312bad494776f855f4ac65de1989e83b9
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-05-29 20:57:55 -0300

    Atualização da lógica de imprimir numero na tela, aceitando numeros maiores que unidade

--- calculo.js
+++ calculo.js
@@ -2,15 +2,46 @@ var numeros = document.querySelectorAll('.tecla');
 
 var tela = document.querySelector('#tela-calculadora');
 
-console.log(numeros);
+var numero1 = '';
+var numero2 = '';
+var operação = '';
+var resultado = '';
+tela.value = numero1
 
+
+
+//Escreve numeros na tela:
 for(var i = 0; i < numeros.length; i++){
     numeros[i].addEventListener('click', function(){
-        if(this.value != 'C'){
-            tela.value = this.value;
-            
-        }else{
-            tela.value = '';
+
+        limpaTela(this.value, 'C');
+
+        if(operação == '' && this.value != 'C'){
+            numero1 = numero1 + this.value;
+            atualizaTela();
+        }else if(operação != '' && this.value != 'C'){
+            numero2 = numero2 + this.value
+            atualizaTela();
         }
     })
+}
+
+
+function atualizaTela(){
+
+    tela.value = numero1 + ' ' + operação + ' ' + numero2;
+
+
+}
+
+function limpaTela(botao, apagar){
+
+    if(botao == apagar){
+        numero1 = '';
+        numero2 = '';
+        operação = '';
+    }
+
+    atualizaTela();
+
 }
