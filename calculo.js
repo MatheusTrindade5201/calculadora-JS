2a57a03d140f27cc99eebfc7907260516a85a05f
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-05-29 19:32:04 -0300

    Imprimir o valor da tecla na tela

---
+++ calculo.js
@@ -0,0 +1,14 @@
+var numeros = document.querySelectorAll('.tecla');
+
+var tela = document.querySelector('#tela-calculadora');
+
+console.log(numeros);
+
+for(var i = 0; i < numeros.length; i++){
+    numeros[i].addEventListener('click', function(){
+        if(this.value != 'C'){
+            tela.value = this.value;
+            
+        }
+    })
+}
